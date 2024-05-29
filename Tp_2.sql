567a09ef032281d0bd45214b5d4af254d3b462a4
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-05-29 19:56:05 +0100

    update

--- Tp_2.sql
+++ Tp_2.sql
@@ -3,9 +3,9 @@ CREATE DATABASE Sans_succes
 use Sans_succes
 
 CREATE TABLE tClients (
--   numCli INT NOT NULL,
+    numCli INT NOT NULL,
     nom VARCHAR(30) NOT NULL, 
---	postNom VARCHAR(30) NOT NULL,
+    postNom VARCHAR(30) NOT NULL,
 --	prenom VARCHAR(30) NOT NULL,
 --	adresse VARCHAR(100),
 --	boitePostale VARCHAR(200),

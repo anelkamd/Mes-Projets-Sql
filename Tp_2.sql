026678c6a0a75e0913cb41cc13636d9b2900053d
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-05-29 19:56:44 +0100

    update

--- Tp_2.sql
+++ Tp_2.sql
@@ -6,9 +6,9 @@ CREATE TABLE tClients (
     numCli INT NOT NULL,
     nom VARCHAR(30) NOT NULL, 
     postNom VARCHAR(30) NOT NULL,
---	prenom VARCHAR(30) NOT NULL,
---	adresse VARCHAR(100),
---	boitePostale VARCHAR(200),
---	ville VARCHAR(50),
---	phone INT
---)
+    prenom VARCHAR(30) NOT NULL,
+    adresse VARCHAR(100),
+    boitePostale VARCHAR(200),
+    ville VARCHAR(50),
+    phone INT
+    )
